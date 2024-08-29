04f360c8f74d41b80fb0cfa86eed12c51ad07f9b
Apple <apple@Apples-Mac-mini.local>
2024-08-29 12:14:18 +0530

    updated register added extra details

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 //const cors = require('cors');
 // const regg = require('./model/regmodel');
+const morgan = require('morgan');
 const register = require('./route/logroute');
+//const login = require('./route/logroute');
+
+
 const app = express();
-
-
+app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api', register);
+// app.use('/api',login);
+app.use('/api',register);
 app.use('/' ,(req,res)=>{
+    res.send(message);
     console.log('hii vro ');
 })
 
